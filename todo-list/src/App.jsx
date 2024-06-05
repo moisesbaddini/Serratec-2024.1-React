0461eafd4633d8411906f5926179d4531cba3584
Moisés Baddini <moisesbaddini@gmail.com>
2024-06-05 17:51:15 -0300

    Término da inserção de elementos na lista

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -7,16 +7,25 @@ const tarefas = ["useState estados", "useEffect efeitos", "arrow functions"];
 function App() {
   const [lista, setLista] = React.useState(tarefas);
 
-  function clicar() {
-    //javaScript
-    setLista(["item novo"]);
+  function clicar(evento) {
+    const form = new FormData(evento.target.parentElement);
+    const texto = form.get("texto");
+    const concluida = form.get("concluida");
+
+    const novo = lista.concat();
+    novo.push(texto + " " + concluida?.toString());
+    setLista(novo);
   }
 
   return (
     <>
       <h2>Lista de estudos</h2>
 
-      <button onClick={clicar}>Criar</button>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" name="texto" />
+        <input type="checkbox" name="concluida" /> Concluída
+        <button onClick={clicar}>Criar</button>
+      </form>
 
       <ol>
         {lista.map((item, i) => (
